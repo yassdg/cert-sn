f8a50cd725295a1b1f0755bee9f838178aae5d8e
yassdg <dieng.yacine@ugb.edu.sn>
2016-01-24 14:30:41 +0000

    pdf generee avec warning

--- src/Cert/IncidentBundle/Controller/GenerePDFController.php
+++ src/Cert/IncidentBundle/Controller/GenerePDFController.php
@@ -9,13 +9,26 @@ class GenerePDFController extends Controller
     public function genererpdfAction()
     {
         $em     = $this->getDoctrine()->getManager();
-        $entity = $em->getRepository('CertIncidentBundle:Vulnerabilite')->find(19);
+        $entity = $em->getRepository('CertIncidentBundle:Vulnerabilite')->find(18);
+
         $html   = $this->renderView('CertIncidentBundle:IncidentFront:vulnerabilite_content.html.twig',
+            array(
+                'entity'=>$entity
+            )
+        );
+
+        $this->get('knp_snappy.pdf')->generateFromHtml(
+
+            $this->renderView(
+                'CertIncidentBundle:IncidentFront:vulnerabilite_content.html.twig',
                 array(
                     'entity'=>$entity
                 )
-            );
-   
+            ),
+            './uploads/documents/'.$entity->getId().'.pdf'
+        );
+        return new Response("Generation okkk !!!"
+            //header('location: http://www.google.com');
+        );
     }
-
 }
